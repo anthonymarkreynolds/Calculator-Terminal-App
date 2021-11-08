05624788032ca7bcaedfa29aa4fbd0a5cc05e60b
m0n4d1-1000 <anthonymarkreynolds@outlook.com>
2021-11-08 14:43:40 +1000

    add baisc input loop and validation

--- app.rb
+++ app.rb
@@ -1,3 +1,5 @@
+require 'io/console'
+
 class Model # crunchs numbers
   def initialize
     @expression = [] # store expression values
@@ -7,26 +9,56 @@ class Model # crunchs numbers
 
   end
 
+  def getExpression
+    @expression
+  end
+
+  def addValue(value)
+    @expression.push(value)
+  end
+
 end
 
 class Controller # handles input
   def initialize
-    @state = new Model
+    @validInputChars = ['1','2','3','4','5','6','7','8','9','0','+','-','*','/','\r','q']
+    @state = 'init'
+    @CalcUI = View.new
   end
 
-  def getInput
+  def step
+
+    # get input character
+    inputChar = STDIN.getch
+
+    # validate input
+    isValid = @validInputChars.include?(inputChar)
+
+    if inputChar == 'q'
+      exit
+    end
+
+    @CalcUI.render(inputChar, isValid)
 
   end
 
-  def validate #validate input
-
+  def start
+    @state = 'running'
+    while @state != 'stopped'
+      step
+    end
   end
-
 end
 
 class View # renders calculator
 
-  def render # render view
-    puts ""
+  def render(inputChar, isValid) # render view
+    puts "Keypressed: #{inputChar} was #{isValid ? "valid" : "invalid"}"
   end
 end
+
+calculatorModel = Model.new
+calculatorControler = Controller.new
+calculatorView = View.new
+
+calculatorControler.start
