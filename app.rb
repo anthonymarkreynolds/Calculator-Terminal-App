384a08a45f5e270ca0db9c76684fb65a5feede70
m0n4d1-1000 <anthonymarkreynolds@outlook.com>
2021-11-08 13:40:42 +1000

    add basic mvc structure

---
+++ app.rb
@@ -0,0 +1,32 @@
+class Model # crunchs numbers
+  def initialize
+    @expression = [] # store expression values
+  end
+
+  def evaluate # evaluate expression
+
+  end
+
+end
+
+class Controller # handles input
+  def initialize
+    @state = new Model
+  end
+
+  def getInput
+
+  end
+
+  def validate #validate input
+
+  end
+
+end
+
+class View # renders calculator
+
+  def render # render view
+    puts ""
+  end
+end
