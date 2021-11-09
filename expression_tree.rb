0486ab659358a1581ae9f3149829e78ec27e6745
m0n4d1-1000 <anthonymarkreynolds@outlook.com>
2021-11-10 08:22:59 +1000

    more work done on expression evaluator

--- expression_tree.rb
+++ expression_tree.rb
@@ -1,49 +1,108 @@
-testExpr = "1+2*3+4"
+test_expr = "1+2*3+4"
 
-class Node
-  def initialize(value = nil)
+class Tree
+
+  def initialize(value)
     @value = value
     @left = nil
     @right = nil
   end
 
-  def insertLeft(value)
-    @left = Node.new(value)
+  def value
+    return @value
   end
 
-  def insertRight(value)
-    @right = Node.new(value)
+  def left
+    @left
+  end
+
+  def right
+    @right
+  end
+
+  def insert_left(value)
+    @left = Tree.new(value)
+  end
+
+  def insert_right(value)
+    @right = Tree.new(value)
   end
 end
 
-def exprTree(expr)
+# build an expression tree
+def build_expr_tree(expr)
   puts expr
 
-  # if expression contains non-numbers
+  # if expression contains non-numbers (build a node)
   if ['+','-','*','/'].map{ |operator| expr.include?(operator) }.reduce(:|)
+
+    # locate last, least precedent operator index
     add = expr.rindex('+')
     min = expr.rindex('-')
 
+    # choose least precedent operator (addition or subtraction )
     if add || min
-      op = add && min ? [add, min].max : add || min
+      op_pos = add && min ? [add, min].max : add || min
+
+    # locate higher precedent operator index
     else
       mul = expr.rindex('*')
       div = expr.rindex('/')
-      op = mul && div ? [mul, div].max : mul || div
+
+      # choose least precedent operator (addition or subtraction )
+      op_pos = mul && div ? [mul, div].max : mul || div
     end
 
-    puts expr[op]
-    left = exprTree(expr[0..(op - 1)])
-    right = exprTree(expr[(op+1)..-1])
-    return Node.new(expr[op]).insertLeft(left).insertRight(right)
+    # get operator value
+    operator =  expr[op_pos]
 
+    # get left expression value
+    left_expr = build_expr_tree(expr[0..(op_pos - 1)])
+
+    # get right expressino value
+    right_expr = build_expr_tree(expr[(op_pos+1)..-1])
+
+    expr_tree = Tree.new(operator)
+    expr_tree.insert_left(left_expr)
+    expr_tree.insert_right(right_expr)
+
+    # return the expression tree root node
+    return expr_tree
+
+  # else expr is a number
   else
     return expr
   end
 end
 
-def evaluate(exprTree)
-
+def bin_fn(token,a,b)
+  a = a.to_f
+  b = b.to_f
+  case token
+  when '+'
+    a + b
+  when '-'
+    a - b
+  when '*'
+    a * b
+  when '/'
+    a / b
+  else
+    'err'
+  end
 end
 
-puts exprTree(testExpr)
+def evaluate(expr_tree)
+
+  # if has child nodes (build_expr_tree is a complete tree, so; if one node exists there are two)
+  unless expr_tree.left || expr_tree.right
+
+  # return leaf value as float
+  else
+    expr_tree.value
+  end
+end
+
+test = build_expr_tree(test_expr)
+
+evaluate(test)
